20edc3fbde551c6e67e02e1d216e4da54b0ca6d8
AvronGols <avrongols.personal@gmail.com>
2022-05-09 07:20:48 +0300

    Updates

--- index.js
+++ index.js
@@ -122,128 +122,68 @@ if (document.querySelector('.map')) {
         document.querySelector('.section-footer__map').style.display = 'none';
     }
 }
-if (document.querySelector(".lightbox__modal")) {
+if (document.querySelector(".modal")) {
 
-    const modal = document.querySelector(".lightbox__modal");
-    const modalContent = document.querySelector(".lightbox__modal-content");
-    const btnClose = document.querySelector(".lightbox__btn-close");
-    const btnPrev = document.querySelector(".lightbox__btn-prev");
-    const btnNext = document.querySelector(".lightbox__btn-next");
+    const openButtons = document.querySelectorAll(".button-open-form");
 
-    const previews = document.querySelectorAll(".button-open-form");
-    const slides = document.querySelectorAll(".lightbox__slide");
+    const modal = document.querySelector(".modal");
+    const modalContent = document.querySelector(".modal__content");
 
-    let currentSlideIndex;
-    let element, bbox, startX, deltaX, raf;
 
-    function isModalActive() {
-        return modal.classList.contains('lightbox__modal_active');
+    const isModalActive = (e) => {
+        return modal.classList.contains('modal_active');
     }
 
-    function openModal() {
-        modal.classList.add('lightbox__modal_active');
+    const openModal = (e) => {
+        modal.classList.add('modal_active');
+        document.documentElement.style.overflow = 'hidden';
     }
 
-    function closeModal() {
-        modal.classList.remove('lightbox__modal_active');
-        slides[currentSlideIndex].classList.remove('lightbox__slide_active');
+    const closeModal = (e) => {
+        modal.classList.remove('modal_active');
+        document.documentElement.style.overflow = 'auto'
     }
 
-    function setCurrentSlide(n) {
-        showSlides(currentSlideIndex = n);
-    }
-
-    function plusCurrentSlide(n) {
-        if (slides.length > 1) {
-            showSlides(currentSlideIndex += n);
-        } else {
-            closeModal();
-        }
-    }
-
-    function showSlides(n) {
-        if (n > slides.length - 1) { currentSlideIndex = 0 }
-        if (n < 0) { currentSlideIndex = slides.length - 1 }
-        for (let i = 0; i < slides.length; i++) {
-            slides[i].classList.remove('lightbox__slide_active');
-        }
-        slides[currentSlideIndex].classList.add('lightbox__slide_active');
-    }
-
-    previews.forEach((preview, i) => {
-        preview.addEventListener('click', () => {
+    openButtons.forEach((button) => {
+        button.addEventListener('click', () => {
             openModal();
-            setCurrentSlide(i);
         });
     });
 
-    slides.forEach(slide => {
-        slide.ondragstart = () => { return false };
-    })
-
-    btnClose.addEventListener('click', closeModal);
+    modal.addEventListener('click', (e) => {
+        if (!modalContent.contains(e.target)) {
+            closeModal();
+        }
+    });
     document.addEventListener('keydown', function(e) {
         if (isModalActive() && e.key === "Escape") {
             closeModal();
         }
     });
 
-    btnPrev.addEventListener('click', () => { plusCurrentSlide(-1) });
-    document.addEventListener('keydown', function(e) {
-        if (isModalActive() && e.key === "ArrowLeft") {
-            plusCurrentSlide(-1);
-        }
-    });
+    let inputs = document.querySelectorAll('.modal__input-files');
+        Array.prototype.forEach.call(inputs, function(input) {
+            let label = input.nextElementSibling,
+    		labelVal = label.innerHTML;
 
-    btnNext.addEventListener('click', () => { plusCurrentSlide(1) });
-    document.addEventListener('keydown', function(e) {
-        if (isModalActive() && e.key === "ArrowRight") {
-            plusCurrentSlide(1);
-        }
-    });
-
-    function userPressed(e) {
-        element = e.target;
-        if (element.classList.contains('lightbox__slide')) {
-            startX = e.clientX;
-            bbox = element.getBoundingClientRect();
-            modalContent.addEventListener('pointermove', userMoved, { passive: true });
-            modalContent.addEventListener('pointerup', userReleased, { passive: true });
-            modalContent.addEventListener('pointercancel', userReleased, { passive: true });
-        };
-    };
-      
-    function userMoved(e) {
-        if (!raf) {
-            deltaX = e.clientX - startX;
-            raf = requestAnimationFrame(userMovedRaf);
-        }
-    };
-      
-    function userMovedRaf() {
-        element.style.transform = "translate3d(" + deltaX + "px, 0px, 0px)";
-        raf = null;
-    };
-      
-    function userReleased() {
-        modalContent.removeEventListener('pointermove', userMoved);
-        modalContent.removeEventListener('pointerup', userReleased);
-        modalContent.removeEventListener('pointercancel', userReleased);
-        if (raf) {
-            cancelAnimationFrame(raf);
-            raf = null;
-        };
-        element.style.left = bbox.left + deltaX + "px";
-        element.style.transform = "translate3d(0px, 0px, 0px)";
-        if (Math.abs(deltaX) > 50) {
-            if (deltaX > 0) {
-                plusCurrentSlide(-1);
-            } else {
-                plusCurrentSlide(1);
+    	input.addEventListener('change', function(e) {
+    		let fileName = '';
+            if (this.files && this.files.length > 0) {
+                fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
             }
-        }
-        deltaX = null;
-    };
+    		// if (this.files && this.files.length > 1) {
+            //     fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+            //     label.style.whiteSpace = 'nowrap';
+            // } else {
+            //     fileName = e.target.value.split("\\").pop();
+            //     label.style.whiteSpace = 'normal';
+            // }
 
-    modalContent.addEventListener('pointerdown', userPressed, { passive: true });
+    		if (fileName) {
+                label.innerHTML = fileName;
+            } else {
+                label.innerHTML = labelVal;
+            }
+    	});
+    });
 }
